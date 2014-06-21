71755f1d89e8759862249037ba077f471ef8ca72
Sterling Hanenkamp <sterling.hanenkamp@bizowie.com>
2014-06-21 17:02:35 -0500

    Convert to the new ruleset syntax

--- cpanm/rules.pl
+++ cpanm/rules.pl
@@ -1,26 +1,34 @@
 # colorize cpanm output
-[
-    qr{--> Working on (\S+)},
-    [ qw( maintask module ) ],
+ruleset {
+    rule qr{--> Working on (\S+)}, qw( 
+        maintask module 
+    );
 
-    qr{(\S+) is up to date. \(([^)]+)\)},
-    [ qw( uptodate module version ) ],
+    rule qr{(\S+) is up to date. \(([^)]+)\)}, qw( 
+        uptodate module version 
+    );
 
-    qr{Fetching (\S+) \.\.\. (\S+)},
-    [ qw( fetchtask url status ) ],
+    rule qr{Fetching (\S+) \.\.\. (\S+)}, qw( 
+        fetchtask url status 
+    );
 
-    qr{Configuring (\S+) \.\.\. (\S+)},
-    [ qw( configtask release status ) ],
+    rule qr{Configuring (\S+) \.\.\. (\S+)}, qw( 
+        configtask release status 
+    );
 
-    qr{Building and testing (\S+) \.\.\. (\S+)},
-    [ qw( buildtask release status ) ],
+    rule qr{Building and testing (\S+) \.\.\. (\S+)}, qw( 
+        buildtask release status 
+    );
 
-    qr{Successfully installed (\S+)},
-    [ qw( success release ) ],
+    rule qr{Successfully installed (\S+)}, qw( 
+        success release 
+    );
 
-    qr{==> Found dependencies: (.*)},
-    [ qw( founddeps modulelist ) ],
+    rule qr{==> Found dependencies: (.*)}, qw( 
+        founddeps modulelist 
+    );
 
-    qr{(\d+) distributions installed},
-    [ qw( distsinstalled counter ) ],
-]
+    rule qr{(\d+) distributions installed}, qw( 
+        distsinstalled counter 
+    );
+}
